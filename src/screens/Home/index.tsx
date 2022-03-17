38eee7a76f23626150b2e5ac835a0e1c1d8ad224
winicius <wine.xavier@gmail.com>
2022-03-16 22:24:25 -0300

    feature: initial commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,13 @@
+// Dependencies
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function Home() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Salve família daquele jeito</Text>
+    </View>
+  );
+}
+
+export default Home;
---
+++ src/styles/theme.ts
@@ -0,0 +1,29 @@
+export default {
+  colors: {
+    header: '#1B1B1F',
+    background_primary: '#f4f5f6',
+    background_secondary: '#FFFFFF',
+
+    text: '#7A7A80',
+    text_detail: '#AEAEB3',
+    title: '#474740',
+
+    line: '#EBEBFB',
+
+    main: '#DC1637',
+    main_light: '#FDEDEF',
+    success: '#03B252',
+
+    shape: '#E1E1E8',
+    shape_dark: '#29292E',
+  },
+
+  fonts: {
+    primary_400: 'Inter_400Regular',
+    primary_500: 'Inter_500Medium',
+
+    secondary_400: 'Inter_400Regular',
+    secondary_500: 'Archivo_500Medium',
+    secondary_600: 'Archivo_600SemiBold',
+  },
+};
